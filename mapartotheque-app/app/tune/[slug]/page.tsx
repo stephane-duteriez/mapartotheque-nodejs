9ea4495872c3f65e78483293b238c0795b9f4eae
Stephane Dateriez <stephane@audiowizard.fr>
2025-04-18 07:22:01 +0000

    try to fix the build

--- mapartotheque-app/app/tune/[slug]/page.tsx
+++ mapartotheque-app/app/tune/[slug]/page.tsx
@@ -1,12 +1,26 @@
-import { fetchById } from "@/firestore/db";
-import { Tune } from "@/type";
+import { fetchAll, fetchById } from "@/firestore/db"
+import { Tune } from "@/type/tune"
+import Image from "next/image"
 
-export const TunePage = async ({ params }: { params: { slug: string } }) => {
-	const tune = await fetchById<Tune>("tunes", params.slug);
-	console.log(tune);
+
+export async function generateStaticParams() {
+	const tunes = await fetchAll<Tune>('tunes')
+	return tunes.map((tune: Tune) => ({ slug: tune.id }))
+}
+
+
+const getTune = async (slug: string) => {
+	const tune = await fetchById<Tune>("tunes", slug)
+	return tune
+}
+
+
+
+const TunePage = async (props: {params: Promise<{slug: string}>}) => {
+	const tune = await getTune((await props.params).slug)
 	return <div>
 		<h1>{tune?.name}</h1>
-		<img src={tune?.imageUrl} alt={tune?.name} />
+		<Image src={tune?.imageUrl ?? ""} alt={tune?.name ?? ""} width={1400} height={500} />
 	</div>;
 }
 
--- mapartotheque-app/server.ts
+++ mapartotheque-app/server.ts
@@ -9,7 +9,8 @@ app.prepare().then(() => {
 	const server = express();
 	server.use("/api", api);
 	server.all("*", (req: express.Request, res: express.Response) => handle(req, res));
-	server.listen(3000, () => {
+	const port = process.env.PORT || 8080;
+	server.listen(port, () => {
 		console.log("Server is running on port 3000");
 	});
 }).catch((err: Error) => {
